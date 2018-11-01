3460998226dc5975e434a18ce1b18e537c04a444
Jos Verlinde[MSFT] <jos_verlinde@hotmail.com>
2018-11-01 14:02:27 +0100

    improve mount SD card

--- Labs/Lab-4.2 SD/SD.py
+++ Labs/Lab-4.2 SD/SD.py
@@ -1,12 +1,17 @@
+# mount SDCard 
+# SDCard configuration for M5Stack
 import uos as os
 
 def mountsd(addlib=True):
     #use SDCard 
     # SDCard configuration for M5Stack
-    #todo check if device is already mounted
-    #   os.umountsd()
-    os.sdconfig(os.SDMODE_SPI, clk=18, mosi=23, miso=19, cs=4)
-    os.mountsd()
+    try:
+        #crude way to detect if the sd is already loaded 
+        _ = os.stat('/sd')
+    except:
+        #if 
+        _ = os.sdconfig(os.SDMODE_SPI, clk=18, mosi=23, miso=19, cs=4)
+        _ = os.mountsd()
 
     if addlib: 
         #very simple , just try to create and add the /sd/lib folder to the path
