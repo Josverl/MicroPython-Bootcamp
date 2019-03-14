b3bf7a220ef2ed23d3dc5518f8e445b403143eaa
Jos Verlinde <jos_verlinde@hotmail.com>
2019-03-14 22:09:03 +0100

    remove lobo

--- Prerequisites/Firmware/MicroPython_LoBo_esp32_psram_all/esp32_psram_all/flash.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-esptool.py --chip esp32 --port /dev/ttyS20 erase_flash
-esptool.py --chip esp32 --port /dev/ttyS20 --baud 921600 --before default_reset --after no_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 bootloader/bootloader.bin 0xf000 phy_init_data.bin 0x10000 MicroPython.bin 0x8000 partitions_mpy.bin
